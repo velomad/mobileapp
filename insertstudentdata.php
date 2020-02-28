8321ce0289088fa8d70c9903eac7e6b21da25e04
velomad <48247308+velomad@users.noreply.github.com>
2020-02-28 10:22:47 +0530

    updatation of query done

--- insertstudentdata.php
+++ insertstudentdata.php
@@ -2,7 +2,7 @@
 
 session_start();
 include('db.php');  
-
+$p_Id=$_SESSION['projectid'];
 $sql = "SELECT * FROM items WHERE project_id=" .$_SESSION['projectid'];
 
 $result = mysqli_query($conn, $sql);
@@ -25,6 +25,8 @@ if(isset($_POST['submit'])){
     $selecthouse = $_POST['selecthouse'];
     $phonenumber = $_POST['phonenumber'];
     $row_no=$_POST['itemno'];
+   
+   
 }
 
 
@@ -36,12 +38,30 @@ mysqli_query($conn, $sql2);
 
 for($i=1; $i<=$row_no;$i++){
 
-    $itemname=$_POST['itemname'.$i];
+    $itemname=$_POST['itemname'.$i ];
     $selectsize=$_POST['selectsize'.$i];
-    $quantity=$_POST['quantity'.$i];
+    $quantity=$_POST['quantity'.$i];    
 
-  echo  $sql3 = "INSERT INTO sizeinfo (stud_id, item_name, size, quantity) VALUES ($last_id, '$itemname', $selectsize, $quantity);";
+    $sql3 = "INSERT INTO sizeinfo (stud_id, item_name, size, quantity) VALUES ($last_id, '$itemname', $selectsize, $quantity);";
         mysqli_query($conn, $sql3);
+
+        $getPiecesql="select * from items where item_name='$itemname' and project_id=$p_Id  ";
+        $resultGetPiece=mysqli_query($conn, $getPiecesql);
+        $rowPiece=mysqli_fetch_assoc($resultGetPiece);
+    for($j=1;$j<=15;$j++){
+        if($rowPiece['s'.$j] == $selectsize){
+                $p_no=$j;
+                break;
+                
+        }
+    }
+    $p="p".$p_no;
+    $oldPieces=$rowPiece['p'.$p_no];
+
+$newQuantity=$oldPieces-$quantity;
+
+$updateSql="update items set $p=$newQuantity where item_name='$itemname' and project_id=$p_Id"  ;
+mysqli_query($conn, $updateSql);
 }
 
 header('Location:u_distribution.php');
